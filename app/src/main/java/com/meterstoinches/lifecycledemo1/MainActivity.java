aebfbaa9b8b84cbfac2f4e81de34393f66238e41
Dinesh Nanda <dannynanda76@gmail.com>
2019-02-26 23:41:16 -0500

    Initial commit

---
+++ app/src/main/java/com/meterstoinches/lifecycledemo1/MainActivity.java
@@ -0,0 +1,59 @@
+package com.meterstoinches.lifecycledemo1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    String tag = "Event";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(tag,"onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(tag,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(tag,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(tag,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(tag,"onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(tag,"onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(tag,"onDestroy");
+    }
+
+    public void click(View view) {
+        Intent s = new Intent(MainActivity.this,Main2Activity.class);
+        startActivity(s);
+    }
+}
